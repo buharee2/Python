ad3f790cb9eef812783b9888c390b40d7cb22f21
Buhari Aminu Adam <buhareeameenu2@gmail.com>
2022-01-11 22:32:34 +0100

    removed the dot after 'OK'

--- disk_usage.py
+++ disk_usage.py
@@ -20,5 +20,5 @@ def check_disk_usage(disk, min_absolute, min_percentage):
 if not check_disk_usage(disk, 2, 10):
     print("Errro! Not enough disk space.")
     sys.exit(1)
-print("Everything is OK.")
+print("Everything is OK")
 sys.exit(0)
